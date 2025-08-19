a0992d3eb976fcdd1928493c846986aa291c0dce
Elena Savchuk <Elena.v.kosenok@gmail.com>
2025-08-19 22:53:12 +0300

    Добавлен файл sender_stand_request.py и перенесены функции функции-запросы (create_order(), get_order_by_track(track_id)). Добавлен обновленный скриншот о прохождении теста

--- configuration.py
+++ configuration.py
@@ -1,5 +1,5 @@
 # URL_SERVICE хранит базовый URL веб-сервиса, который используется для доступа к API или другим ресурсам.
-URL_SERVICE = " https://bdf7b233-ba33-4d9a-90a8-6e2542f37ea0.serverhub.praktikum-services.ru"
+URL_SERVICE = " https://3b65b1db-bf20-4e89-8435-4f7b666573ae.serverhub.praktikum-services.ru"
 
 # CREATE_ORDER_PATH хранит путь к API-методу для создания нового заказа.
 CREATE_ORDER_PATH = "/api/v1/orders"
--- scooter_rent_test.py
+++ scooter_rent_test.py
@@ -1,40 +1,16 @@
 # Елена Савчук, 33-я когорта, Дипломный проект. Инженер по тестированию плюс
 
-import configuration
-import data
+import sender_stand_request
 
-# Импорт библиотеки requests для выполнения HTTP-запросов
-import requests
-
-# Функция создания заказа
-
-
-def create_order():
-    res = requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER_PATH,
-                        json=data.order_body,
-                        headers=data.headers)
-    json = res.json()
-    return json.get('track')
-
-# Функция получения заказа по его номеру
-
-
-def get_order_by_track(track_id):
-    path = configuration.URL_SERVICE + \
-        configuration.GET_ORDER_BY_TRACK_PATH + str(track_id)
-    print(path)
-    return requests.get(path, data.headers)
 
 # Функция тестирования создания заказа и получения по треку
-
-
 def test_scooter_rent():
     # 1. Создание заказа и получение трека
-    track_id = create_order()
+    track_id = sender_stand_request.create_order()
     print(track_id)
 
     # 2. Получение заказа по его номеру
-    res = get_order_by_track(track_id)
+    res = sender_stand_request.get_order_by_track(track_id)
 
     # 3. Проверка, что код ответа равен 200.
     assert res.status_code == 200
